f4146e677f97583578f526224c3d6001521f9840
Willixrain <167516635+itismygo@users.noreply.github.com>
2025-06-16 22:49:11 +0800

    Update m2i.py

--- m2i.py
+++ m2i.py
@@ -9,7 +9,8 @@ def m2ipy(md_text):
     html_content = convert_markdown_to_html(md_text)
     logger.info("转化成html成功")
     # 渲染并截图
-    output_path = StarTools.get_data_dir('output.png')
+    output_dir = StarTools.get_data_dir()
+    output_path = output_dir / "output.png"
     render_and_screenshot(html_content,output_path)
 
     logger.info("截图成功")
